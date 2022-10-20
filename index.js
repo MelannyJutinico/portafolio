6e5c89fac704f83722bf49b00f33fe098423de64
MelannyJutinico <melannyjutinico@gmail.com>
2022-10-20 01:49:35 -0500

    portfolio almost finish

--- index.js
+++ index.js
@@ -3,40 +3,38 @@ const app = express()
 const port = 3000
 
 const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs')
-app.set('views',__dirname+'/views')
+app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
-  //res.send('Pagina principal')
-  console.log(__dirname)
-  res.render('index')
+    //res.send('Pagina principal')
+    console.log(__dirname)
+    res.render('index')
 })
 app.get('/educacion', (req, res) => {
-  res.render('academico', {
-    titulo:"Seccion educacion",
-    descripcion:"Aqui encontraras mi formacion academica"
-  }
-  )
+    res.render('academico', {
+        titulo: "Seccion educacion",
+        descripcion: "Aqui encontraras mi formacion academica y certificaciones"
+    })
 })
 app.get('/datos', (req, res) => {
-  res.render('datos', {
-    titulo:"Seccion datos basicos",
-    descripcion:"Aqui encontraras nis datos basicos"
-  }
-  )
+    res.render('datos', {
+        titulo: "Seccion datos basicos",
+        descripcion: "Aqui encontraras nis datos basicos y informacion sobre mi"
+    })
 })
-app.get('/reconocimientos', (req, res) => {
-  res.send('Pagina de reconocimientos')
-})
-app.get('/conocimientos', (req, res) => {
-  res.send('Pagina de conocimientos')
+app.get('/skills', (req, res) => {
+    res.render('skills', {
+        titulo: "Skills y portafolio",
+        descripcion: "Aqui encontraras mis skills y algunas entradas de mi portafolio"
+    })
 })
 
-app.use((req,res,next) => {
-  res.status(404).sendFile(__dirname+'/public/404.html')
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/public/404.html')
 })
 
 app.listen(port, () => {
-  console.log(`Acceda al servidor haciendo click aqui http://localhost:${port}`)
+    console.log(`Acceda al servidor haciendo click aqui http://localhost:${port}`)
 })
